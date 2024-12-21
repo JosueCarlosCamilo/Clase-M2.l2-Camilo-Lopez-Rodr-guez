284b7a8bd239e8f144239f8e167992e41917a27b
JosueCarlosCamilo <camilo12lopezro@gmail.com>
2024-12-20 21:25:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,21 @@
-import discord # Libreria de Discord
-from psw import gen_pass #from permite importar otros archivos a tu código
-from psw import gen_emodji
-from psw import flip_coin
-from clasebot import*
-# La variable intents almacena los privilegios del bot
+import discord
+from discord.ext import commands
+from funciones import *
+
 intents = discord.Intents.default()
-# Activar el privilegio de lectura de mensajes
 intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready(): #comando que enciende el bot de discord
-    print('Hemos iniciado sesión como {client.user}')
-
-# Cuando el bot reciba un mensaje, ¡enviará mensajes en el mismo canal!
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('¡Hola Soy un bot!')
-    elif message.content.startswith('$bye'):
-        await message.channel.send('adios')
-    elif message.channel.startswith('$smile'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('$psw'):
-        await message.channel.send('aqui tienes la cintrasena = '+ gen_pass(8))
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    else:
-        await message.channel.send("No puedo procesar tu código, ¡Lo siento!")
-
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    
+@bot.command()
+async def descomponer(ctx, *, objeto:str):
+    #llamar la funcion
+    result = tiempo_descomp(objeto)
+    
+    #Enviar respuesta
+    await ctx.send(result)
+    
 token = ""
-
-client.run(token)
+bot.run(token)
